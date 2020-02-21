5e7864afcd4ea32562b511dbf221d2b16ec81564
github username <github email address>
2020-02-21 14:51:32 -0500

    push to deploy

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import '../styles/Footer.css'
 
 export default function Footer() {
-  return <div className="footer">DWBC NYC • The Manhattan Project, 2020</div>
+  return <div className="footer">DWBC NYC • The Manhattan Project • 2020</div>
 }
