3bd2030072a349b4c1996c4a63735f0f68c3f1c8
github username <github email address>
2020-02-21 22:53:45 -0500

    links in Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,5 +2,16 @@ import React from 'react'
 import '../styles/Footer.css'
 
 export default function Footer() {
-  return <div className="footer">DWBC NYC • The Manhattan Project • 2020</div>
+  return (
+    <div className="footer">
+      <a href="http://www.diamondway.org/ny/" target="_new">
+        DWBC NYC
+      </a>{' '}
+      •{' '}
+      <a href="http://www.diamondway.org/manhattan-project/" target="_new">
+        The Manhattan Project
+      </a>{' '}
+      • 2020
+    </div>
+  )
 }
